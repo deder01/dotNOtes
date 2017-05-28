a1f957508d79eb429b7c0f3edf15f338c5ac16f8
David <david.eder26@gmail.com>
2017-05-28 18:56:51 -0400

    Everything works with a whole lot of awesome es6

--- app/components/Github/UserProfile.js
+++ app/components/Github/UserProfile.js
@@ -1,21 +1,19 @@
 import React from 'react';
 
-class UserProfile extends React.Component {
-  render(){
-      return (
-        <div>
-          {this.props.bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
-          {this.props.bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
-          {this.props.bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
-          {this.props.bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
-          {this.props.bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
-          {this.props.bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
-          {this.props.bio.followers >= 0 && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
-          {this.props.bio.following >= 0 && <li className="list-group-item">Following: {this.props.bio.following}</li>}
-          {this.props.bio.blog && <li className="list-group-item">Blog: <a href={this.props.bio.blog}> {this.props.bio.blog}</a></li>}
-        </div>
-      );
-  }
+UserProfile = ({bio}) => {
+  return (
+    <div>
+      {bio.avatar_url && <li className="list-group-item"> <img src={this.props.bio.avatar_url} className="img-rounded img-responsive"/></li>}
+      {bio.name && <li className="list-group-item">Name: {this.props.bio.name}</li>}
+      {bio.login && <li className="list-group-item">Username: {this.props.bio.login}</li>}
+      {bio.email && <li className="list-group-item">Email: {this.props.bio.email}</li>}
+      {bio.location && <li className="list-group-item">Location: {this.props.bio.location}</li>}
+      {bio.company && <li className="list-group-item">Company: {this.props.bio.company}</li>}
+      {bio.followers >= 0 && <li className="list-group-item">Followers: {this.props.bio.followers}</li>}
+      {bio.following >= 0 && <li className="list-group-item">Following: {this.props.bio.following}</li>}
+      {bio.blog && <li className="list-group-item">Blog: <a href={this.props.bio.blog}> {this.props.bio.blog}</a></li>}
+    </div>
+  );
 }
 
 UserProfile.propTypes = {
--- app/components/Notes/Notes.js
+++ app/components/Notes/Notes.js
@@ -2,16 +2,15 @@ import React from 'react';
 import NotesList from './NotesList';
 import AddNote from './AddNote';
 
-class Notes extends React.Component {
-  render() {
-    return (
-      <div>
-        <h3> Notes for {this.props.username} </h3>
-        <AddNote username={this.props.username} addNote={this.props.addNote} />
-        <NotesList notes={this.props.notes} />
-      </div>
-    );
-  }
+
+Notes = ({username, notes, addNote}) => {
+  return (
+    <div>
+      <h3> Notes for {this.props.username} </h3>
+      <AddNote username={this.props.username} addNote={this.props.addNote} />
+      <NotesList notes={this.props.notes} />
+    </div>
+  );
 }
 
 Notes.propTypes = {
